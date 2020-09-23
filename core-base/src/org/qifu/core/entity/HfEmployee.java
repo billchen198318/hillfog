1f7f3cfa507a7f68f22d8d0446a955c748bd4a92
billchen198318 <chen.xin.nien@gmail.com>
2020-09-23 14:00:20 +0800

    update

---
+++ core-base/src/org/qifu/core/entity/HfEmployee.java
@@ -0,0 +1,103 @@
+package org.qifu.core.entity;
+
+import java.util.Date;
+
+import org.qifu.base.model.CreateDateField;
+import org.qifu.base.model.CreateUserField;
+import org.qifu.base.model.EntityPK;
+import org.qifu.base.model.EntityUK;
+import org.qifu.base.model.UpdateDateField;
+import org.qifu.base.model.UpdateUserField;
+
+public class HfEmployee implements java.io.Serializable {
+	private static final long serialVersionUID = 486336952630513992L;
+	
+	private String oid;
+	private String account;
+	private String empId;
+	private String name;
+	private String description;
+	private String cuserid;
+	private Date cdate;
+	private String uuserid;
+	private Date udate;
+	
+	@EntityPK(name = "oid", autoUUID = true)
+    public String getOid() {
+        return oid;
+    }
+    
+    public void setOid(String oid) {
+        this.oid = oid;
+    }
+    
+    @EntityUK(name = "account")
+    public String getAccount() {
+        return account;
+    }
+    
+    public void setAccount(String account) {
+        this.account = account;
+    }
+    
+    public String getEmpId() {
+        return empId;
+    }
+    
+    public void setEmpId(String empId) {
+        this.empId = empId;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public String getDescription() {
+        return description;
+    }
+    
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    
+    @CreateUserField(name = "cuserid")
+    public String getCuserid() {
+        return cuserid;
+    }
+    
+    public void setCuserid(String cuserid) {
+        this.cuserid = cuserid;
+    }
+    
+    @CreateDateField(name = "cdate")
+    public Date getCdate() {
+        return cdate;
+    }
+    
+    public void setCdate(Date cdate) {
+        this.cdate = cdate;
+    }
+    
+    @UpdateUserField(name = "uuserid")
+    public String getUuserid() {
+        return uuserid;
+    }
+    
+    public void setUuserid(String uuserid) {
+        this.uuserid = uuserid;
+    }
+    
+    @UpdateDateField(name = "udate")
+    public Date getUdate() {
+        return udate;
+    }
+    
+    public void setUdate(Date udate) {
+        this.udate = udate;
+    }
+    
+}
