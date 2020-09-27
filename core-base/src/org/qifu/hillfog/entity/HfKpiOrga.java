7424956aa84e4c6dc0857701439da957e7b2071e
billchen198318 <chen.xin.nien@gmail.com>
2020-09-27 15:12:19 +0800

    update

---
+++ core-base/src/org/qifu/hillfog/entity/HfKpiOrga.java
@@ -0,0 +1,86 @@
+package org.qifu.hillfog.entity;
+
+import java.util.Date;
+
+import org.qifu.base.model.CreateDateField;
+import org.qifu.base.model.CreateUserField;
+import org.qifu.base.model.EntityPK;
+import org.qifu.base.model.EntityUK;
+import org.qifu.base.model.UpdateDateField;
+import org.qifu.base.model.UpdateUserField;
+
+public class HfKpiOrga implements java.io.Serializable {
+	private static final long serialVersionUID = 4105130962182592788L;
+	
+    private String oid;
+    private String kpiId;
+    private String orgId;
+    private String cuserid;
+    private Date cdate;
+    private String uuserid;
+    private Date udate;
+    
+    @EntityPK(name = "oid", autoUUID = true)
+    public String getOid() {
+        return oid;
+    }
+    
+    public void setOid(String oid) {
+        this.oid = oid;
+    }
+    
+    @EntityUK(name = "kpiId")
+    public String getKpiId() {
+        return kpiId;
+    }
+    
+    public void setKpiId(String kpiId) {
+        this.kpiId = kpiId;
+    }
+    
+    @EntityUK(name = "orgId")
+    public String getOrgId() {
+        return orgId;
+    }
+    
+    public void setOrgId(String orgId) {
+        this.orgId = orgId;
+    }
+    
+    @CreateUserField(name = "cuserid")
+    public String getCuserid() {
+        return cuserid;
+    }
+    
+    public void setCuserid(String cuserid) {
+        this.cuserid = cuserid;
+    }
+    
+    @CreateDateField(name = "cdate")
+    public Date getCdate() {
+        return cdate;
+    }
+    
+    public void setCdate(Date cdate) {
+        this.cdate = cdate;
+    }
+    
+    @UpdateUserField(name = "uuserid")
+    public String getUuserid() {
+        return uuserid;
+    }
+    
+    public void setUuserid(String uuserid) {
+        this.uuserid = uuserid;
+    }
+    
+    @UpdateDateField(name = "udate")
+    public Date getUdate() {
+        return udate;
+    }
+    
+    public void setUdate(Date udate) {
+        this.udate = udate;
+    }
+	
+}
