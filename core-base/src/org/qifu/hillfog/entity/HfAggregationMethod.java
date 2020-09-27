dfe273ebac773cf93c91313817c3da44fbac91ca
billchen198318 <chen.xin.nien@gmail.com>
2020-09-27 16:16:50 +0800

    update

---
+++ core-base/src/org/qifu/hillfog/entity/HfAggregationMethod.java
@@ -0,0 +1,121 @@
+package org.qifu.hillfog.entity;
+
+import java.util.Date;
+
+import org.qifu.base.model.CreateDateField;
+import org.qifu.base.model.CreateUserField;
+import org.qifu.base.model.EntityPK;
+import org.qifu.base.model.EntityUK;
+import org.qifu.base.model.UpdateDateField;
+import org.qifu.base.model.UpdateUserField;
+
+public class HfAggregationMethod implements java.io.Serializable {
+	private static final long serialVersionUID = 1362267676918464688L;
+	
+    private String oid;
+    private String aggrId;
+    private String name;
+    private String type;
+    private String expression1;
+    private String expression2;
+    private String description;
+    private String cuserid;
+    private Date cdate;
+    private String uuserid;
+    private Date udate;
+    
+    @EntityPK(name = "oid", autoUUID = true)
+    public String getOid() {
+        return oid;
+    }
+    
+    public void setOid(String oid) {
+        this.oid = oid;
+    }
+    
+    @EntityUK(name = "aggrId")
+    public String getAggrId() {
+        return aggrId;
+    }
+    
+    public void setAggrId(String aggrId) {
+        this.aggrId = aggrId;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public String getType() {
+        return type;
+    }
+    
+    public void setType(String type) {
+        this.type = type;
+    }
+    
+    public String getExpression1() {
+        return expression1;
+    }
+    
+    public void setExpression1(String expression1) {
+        this.expression1 = expression1;
+    }
+    
+    public String getExpression2() {
+        return expression2;
+    }
+    
+    public void setExpression2(String expression2) {
+        this.expression2 = expression2;
+    }
+    
+    public String getDescription() {
+        return description;
+    }
+    
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    
+    @CreateUserField(name = "cuserid")
+    public String getCuserid() {
+        return cuserid;
+    }
+    
+    public void setCuserid(String cuserid) {
+        this.cuserid = cuserid;
+    }
+    
+    @CreateDateField(name = "cdate")
+    public Date getCdate() {
+        return cdate;
+    }
+    
+    public void setCdate(Date cdate) {
+        this.cdate = cdate;
+    }
+    
+    @UpdateUserField(name = "uuserid")
+    public String getUuserid() {
+        return uuserid;
+    }
+    
+    public void setUuserid(String uuserid) {
+        this.uuserid = uuserid;
+    }
+    
+    @UpdateDateField(name = "udate")
+    public Date getUdate() {
+        return udate;
+    }
+    
+    public void setUdate(Date udate) {
+        this.udate = udate;
+    }
+    
+}
