1e0abc24b061dd3c0d914e8ea2d1f5818be51478
Bill Chen <chen.xin.nien@gmail.com>
2021-08-26 15:56:22 +0800

    update

---
+++ core-app/src/META-INF/resource/create-user-data-ldap-mode.groovy
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.math.NumberUtils;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.TransactionCallback;
+import org.springframework.transaction.support.TransactionTemplate;
+
+import org.qifu.base.*;
+import org.qifu.util.*;
+import org.qifu.core.util.*;
+import org.qifu.hillfog.entity.*;
+import org.qifu.hillfog.logic.*;
+
+try {
+	UserUtils.setUserInfoForUserLocalUtilsBackgroundMode();
+	IEmployeeLogicService employeeLogicService = (IEmployeeLogicService) AppContext.context.getBean(IEmployeeLogicService.class);
+	List<String> orgList = new ArrayList<String>();
+	orgList.add("SD"); // no rule for current, modify it.
+	HfEmployee employee = new HfEmployee();
+	employee.setAccount( user.username );
+	employee.setEmpId( SimpleUtils.createRandomString(5).toUpperCase() ); // no rule for current, modify it.
+	employee.setName( user.username );
+	employee.setUploadOid("");
+	employee.setJobTitle( "normal" );
+	employee.setDescription( user.username + " 's role, create by LDAP login!");
+	employeeLogicService.create(employee, user.password, orgList);
+	System.out.println("create account data by LDAP login : " + user.username);
+} catch (Exception e) {
+	throw e;
+} finally {
+	UserUtils.removeForUserLocalUtils();
+}
